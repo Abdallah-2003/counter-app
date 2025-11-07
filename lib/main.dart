62674ec00e364da943e50db9b8f19e64e9a7ace6
Abdallah Khaled <bdallhbdalqadr92@gmail.com>
2025-11-07 10:45:15 +0200

    refactor main.dart file

--- lib/main.dart
+++ lib/main.dart
@@ -1,52 +1,19 @@
 import 'package:flutter/material.dart';
 import 'point_counter.dart';
 
-class AbdallahApp extends StatelessWidget{
+class CounterApp extends StatelessWidget{
   
-  const AbdallahApp({super.key});
+  const CounterApp({super.key});
   
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              Container(
-                width: 100,
-                height: 40,
-                color: Colors.deepPurpleAccent,
-                child: Center(
-                  child: Text('Abdallah')
-                ),
-              ),
-              Container(
-                width: 80,
-                height: 40,
-                color: Colors.deepPurpleAccent,
-                child: Center(
-                  child: Text('Khaled')
-                ),
-              ),
-              Container(
-                width: 80,
-                height: 40,
-                color: Colors.deepPurpleAccent,
-                child: Center(
-                  child: Text('Abdelkader')
-                ),
-              ),
-            ],
-          ),
-        ),
-      ),
+      home: PointCounter(),
     );
   }
 }
 
 
 void main() {
-  runApp(PointCounter());
+  runApp(CounterApp());
 }

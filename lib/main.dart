dc6ba1d035406b7f51f8599d7f11dfc41050b748
Abdallah Khaled <bdallhbdalqadr92@gmail.com>
2025-11-07 19:46:01 +0200

    provide counter cubit

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:myapps1/cubit/counter_cubit.dart';
 import 'point_counter.dart';
 
-class CounterApp extends StatelessWidget{
-  
+class CounterApp extends StatelessWidget {
   const CounterApp({super.key});
-  
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: PointCounter(),
+    return BlocProvider(
+      create: (context) => CounterCubit(),
+      child: MaterialApp(home: PointCounter()),
     );
   }
 }
-
 
 void main() {
   runApp(CounterApp());
--- lib/point_counter.dart
+++ lib/point_counter.dart
@@ -1,201 +1,177 @@
 import 'package:flutter/material.dart';
 
-class PointCounter extends StatefulWidget{
 
-  @override
-  State<PointCounter> createState() => _PointCounterState();
-}
 
-class _PointCounterState extends State<PointCounter> {
-  int ascPoints = 0;
-
-  int zscPoints = 0;
+class PointCounter extends StatelessWidget {
+  
+  const PointCounter({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        appBar: AppBar(
-          backgroundColor: Colors.blueGrey,
-          title: Text('Point Counter',
-            style: TextStyle(
-              color: Colors.white
-            ),
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.blueGrey,
+        title: Text('Point Counter',
+          style: TextStyle(
+            color: Colors.white
           ),
         ),
-        
-        body: Column(
-          children: [
-            Spacer(flex: 1),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                Column(
-                  children: [
-                    Text('ASC',
-                      style: TextStyle(
-                        fontSize: 40
-                      ),
-                    ),
-                    Text('$ascPoints',
-                      style: TextStyle(
-                        fontSize: 180
-                      ),
-                    ),
-                    ElevatedButton(
-                      style: ElevatedButton.styleFrom(
-                        backgroundColor: Colors.blueGrey
-                      ),
-                      onPressed: (){
-                        setState(() {
-                          ascPoints++;
-                        });
-                      }, 
-                      child: Text('Add 1 Point',
-                        style: TextStyle(
-                          color: Colors.white
-                        ),
-                      ),
-                    ),
-                    SizedBox(
-                      height: 16,
-                    ),
-                    ElevatedButton(
-                      style: ElevatedButton.styleFrom(
-                        backgroundColor: Colors.blueGrey
-                      ),
-                      onPressed: (){
-                        setState(() {
-                          ascPoints += 2;
-                        });
-                      }, 
-                      child: Text('Add 2 Point',
-                        style: TextStyle(
-                          color: Colors.white
-                        ),
-                      ),
-                    ),
-                    SizedBox(
-                      height: 16,
-                    ),
-                    ElevatedButton(
-                      style: ElevatedButton.styleFrom(
-                        backgroundColor: Colors.blueGrey
-                      ),
-                      onPressed: (){
-                        setState(() {
-                          ascPoints += 3;
-                        });
-                      }, 
-                      child: Text('Add 3 Point',
-                        style: TextStyle(
-                          color: Colors.white
-                        ),
-                      ),
-                    ),
-                   
-                  ],
-                ),
-                SizedBox(
-                  height: 450,
-                  child: VerticalDivider(
-                    color: Colors.grey,
-                    thickness: 1,
-                  ),
-                ),
-                Column(
-                  children: [
-                    Text('ZSC',
-                      style: TextStyle(
-                        fontSize: 40
-                      ),
-                    ),
-                    Text('$zscPoints',
-                      style: TextStyle(
-                        fontSize: 180
-                      ),
-                    ),
-                    ElevatedButton(
-                      style: ElevatedButton.styleFrom(
-                        backgroundColor: Colors.blueGrey
-                      ),
-                      onPressed: (){
-                        setState(() {
-                          zscPoints++;
-                        });
-                      }, 
-                      child: Text('Add 1 Point',
-                        style: TextStyle(
-                          color: Colors.white
-                        ),
-                      ),
-                    ),
-                    SizedBox(
-                      height: 16,
-                    ),
-                    ElevatedButton(
-                      style: ElevatedButton.styleFrom(
-                        backgroundColor: Colors.blueGrey
-                      ),
-                      onPressed: (){
-                        setState(() {
-                          zscPoints += 2;
-                        });
-                      }, 
-                      child: Text('Add 2 Point',
-                        style: TextStyle(
-                          color: Colors.white
-                        ),
-                      ),
-                    ),
-                    SizedBox(
-                      height: 16,
-                    ),
-                    ElevatedButton(
-                      style: ElevatedButton.styleFrom(
-                        backgroundColor: Colors.blueGrey
-                      ),
-                      onPressed: (){
-                        setState(() {
-                          zscPoints += 3;
-                        });
-                      }, 
-                      child: Text('Add 3 Point',
-                        style: TextStyle(
-                          color: Colors.white
-                        ),
-                      ),
-                    ),
-                   
-                  ],
-                )
-              ],
-            ),
-            SizedBox(
-              height: 100,
-            ),
-            ElevatedButton(
-              style: ElevatedButton.styleFrom(
-                backgroundColor: Colors.blueGrey
-              ),
-              onPressed: (){
-                setState(() {
-                  ascPoints = zscPoints = 0;
-                });
-              },
-              child: Text('Reset',
-                style: TextStyle(
-                  color: Colors.white
-                ),
-              ),
-            ),
-            Spacer(flex: 1),
-          ],
-          
-        ),
-        
       ),
-    );
+      
+      body: Column(
+        children: [
+          Spacer(flex: 1),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Column(
+                children: [
+                  Text('ASC',
+                    style: TextStyle(
+                      fontSize: 40
+                    ),
+                  ),
+                  Text('0',
+                    style: TextStyle(
+                      fontSize: 180
+                    ),
+                  ),
+                  ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.blueGrey
+                    ),
+                    onPressed: (){
+                      
+                    }, 
+                    child: Text('Add 1 Point',
+                      style: TextStyle(
+                        color: Colors.white
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 16,
+                  ),
+                  ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.blueGrey
+                    ),
+                    onPressed: (){
+                      
+                    }, 
+                    child: Text('Add 2 Point',
+                      style: TextStyle(
+                        color: Colors.white
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 16,
+                  ),
+                  ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.blueGrey
+                    ),
+                    onPressed: (){
+                      
+                    }, 
+                    child: Text('Add 3 Point',
+                      style: TextStyle(
+                        color: Colors.white
+                      ),
+                    ),
+                  ),
+                  
+                ],
+              ),
+              SizedBox(
+                height: 450,
+                child: VerticalDivider(
+                  color: Colors.grey,
+                  thickness: 1,
+                ),
+              ),
+              Column(
+                children: [
+                  Text('ZSC',
+                    style: TextStyle(
+                      fontSize: 40
+                    ),
+                  ),
+                  Text('0',
+                    style: TextStyle(
+                      fontSize: 180
+                    ),
+                  ),
+                  ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.blueGrey
+                    ),
+                    onPressed: (){
+                      
+                    }, 
+                    child: Text('Add 1 Point',
+                      style: TextStyle(
+                        color: Colors.white
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 16,
+                  ),
+                  ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.blueGrey
+                    ),
+                    onPressed: (){
+                      
+                    }, 
+                    child: Text('Add 2 Point',
+                      style: TextStyle(
+                        color: Colors.white
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 16,
+                  ),
+                  ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.blueGrey
+                    ),
+                    onPressed: (){
+                      
+                    }, 
+                    child: Text('Add 3 Point',
+                      style: TextStyle(
+                        color: Colors.white
+                      ),
+                    ),
+                  ),
+                  
+                ],
+              )
+            ],
+          ),
+          SizedBox(
+            height: 100,
+          ),
+          ElevatedButton(
+            style: ElevatedButton.styleFrom(
+              backgroundColor: Colors.blueGrey
+            ),
+            onPressed: (){
+              
+            },
+            child: Text('Reset',
+              style: TextStyle(
+                color: Colors.white
+              ),
+            ),
+          ),
+          Spacer(flex: 1),
+        ],
+      )
+    );     
   }
 }
